feb505b600ab333da35a52e16af37d6b52de21a4
yuri.liang <yuri.liang@ericsson.com>
2020-02-09 11:54:00 +0800

    modify vimrc

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@
 
 # install neovim
 mkdir -p $HOME/.local/bin
-wget -O $HOME/.local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-chmod +x $HOME/.local/bin/nvim
-echo "alias vim='nvim'" >> $HOME/.bashrc
+wget -O $HOME/.local/bin/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+chmod +x $HOME/.local/bin/nvim.appimage
+cd $HOME/.local/bin/
+./nvim.appimage --appimage-extract
+rm -rf ./nvim.appimage
+echo "alias vim=$HOME/.local/bin/squashfs-root/usr/bin/nvim" >> $HOME/.bashrc
 source $HOME/.bashrc
+cd ~
 
 # install vimrc
 mkdir -p $HOME/.config/nvim
-cp vimrc $HOME/.config/nvim/
+cp vimrc $HOME/.config/nvim/init.vim
