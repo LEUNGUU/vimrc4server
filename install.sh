1cb66f3bbe50fe559d80067cf8bccfd03d47a221
yuri.liang <yuri.liang@ericsson.com>
2020-02-09 11:39:34 +0800

    add nvim

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 # install neovim
 mkdir -p $HOME/.local/bin
 wget -O $HOME/.local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+chmod +x $HOME/.local/bin/nvim
+echo "alias vim='nvim'" >> $HOME/.bashrc
+source $HOME/.bashrc
 
 # install vimrc
 mkdir -p $HOME/.config/nvim
-mv vimrc $HOME/.config/nvim
+cp vimrc $HOME/.config/nvim/
